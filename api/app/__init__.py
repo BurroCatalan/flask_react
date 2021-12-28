8383ea6379fe03c0ee822bad9f57209de3199887
Kevin Biallas <M0023423@mahle.com>
2021-12-28 14:21:42 +0100

    login

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -2,7 +2,9 @@ import time
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from config import Config
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- api/app/models.py
+++ api/app/models.py
@@ -1,9 +1,12 @@
-from app import db
+from app import db, login
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
 #import json
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     firstName = db.Column(db.String(100), index=False, unique=False)
@@ -12,9 +15,18 @@ class User(db.Model):
     passwordHash = db.Column(db.String(128))
     projectRequests = relationship('ProjectRequest', backref='user', lazy=True)
 
-
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
     #def toJSON(self):
 #	return json.dumps(self, default=lambda o: o.__d sort_keys=True, indent=4)
