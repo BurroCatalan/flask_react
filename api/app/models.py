65ff8d72e7c24c432e0fafa67964c36ff552c60c
Kevin Biallas <M0023423@mahle.com>
2022-01-02 00:57:35 +0100

    passHash2

--- api/app/models.py
+++ api/app/models.py
@@ -21,7 +21,7 @@ class User(db.Model):
         return '<User {}>'.format(self.username)
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.passwordHash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.passwordHash, password)
