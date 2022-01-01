83f279e1d26c93a666f6088f919304c6db8f45a6
Kevin Biallas <M0023423@mahle.com>
2022-01-02 00:48:06 +0100

    errors

--- api/app/auth.py
+++ api/app/auth.py
@@ -1,10 +1,20 @@
 from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from app.models import User
-from app.errors import error_response
+from flask import jsonify
+from werkzeug.http import HTTP_STATUS_CODES
+#from app.errors import error_response
 
 
 basic_auth = HTTPBasicAuth()
 token_auth = HTTPTokenAuth()
+
+def error_response(status_code, message=None):
+    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
+    if message:
+        payload['message'] = message
+    response = jsonify(payload)
+    response.status_code = status_code
+    return response
 
 @basic_auth.verify_password
 def verify_password(username, password):
--- api/app/models.py
+++ api/app/models.py
@@ -1,13 +1,12 @@
-from app import db, login
+from app import db
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
 import base64
 from datetime import datetime, timedelta
 import os
 
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     firstName = db.Column(db.String(100), index=False, unique=False)
@@ -46,9 +45,9 @@ class User(UserMixin, db.Model):
             return None
         return user
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+#@login.user_loader
+#def load_user(id):
+#    return User.query.get(int(id))
 
     #def toJSON(self):
 #	return json.dumps(self, default=lambda o: o.__d sort_keys=True, indent=4)
